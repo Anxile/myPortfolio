20d83f662d9390fdfe07aae6496941940d82808d
Anxile <onebox.chen@outlook.com>
2025-01-24 14:48:34 -0500

    create contact page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import React from 'react';
-import Header from './components/Header/Header';
-import Career from './components/Career/Career';
-import Skills from './components/Skills/Skills';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <main className="welcome">
-        {/* 其他内容 */}
-      </main>
-      <Career />
-      <Skills />
-    </div>
-  );
-}
+
+
 
 export default App;
