29b58f05191796b8b9fbc23c17f94246c9131bd7
Anxile <onebox.chen@outlook.com>
2025-02-05 23:59:42 -0500

    initialize React + TypeScript + Vite for the project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Alert from "./components/alert";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  const [altervisible, setalertvisibility] = useState(false);
+  return (
+    <>
+      {altervisible && (
+        <Alert onClose={() => setalertvisibility(false)}>Alert</Alert>
+      )}
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => setalertvisibility(true)}
+      >
+        Primary
+      </button>
+    </>
+  );
+}
+
+export default App;
