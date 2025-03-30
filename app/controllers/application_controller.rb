cced8e03073fd9e1f32a612f0412c21b7c188b5f
Anxile <onebox.chen@outlook.com>
2025-03-30 14:23:57 -0400

    Implement user session management: add login checks, current user helper, and update views for user greeting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    before_action :ensure_user_logged_in
+    helper_method :current_user, :is_loggedin?
+
+    protected
+    def ensure_user_logged_in
+        redirect_to login_path unless session[:user_id]
+    end
+
+    def is_loggedin?
+        session[:user_id].present?
+    end
+    def current_user
+        @current_user ||= User.find(session[:user_id])
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_user_logged_in, only:[:new, :create]
+
   def new
   end
 
@@ -7,6 +9,7 @@ class SessionsController < ApplicationController
     password = params[:user][:password]
     puts password, user
     if user && user.authenticate(password)
+      session[:user_id] = user.id
       redirect_to users_path, notice: "Logged in!"
     else
       flash.now[:alert] = "Email or password is invalid"
@@ -16,6 +19,7 @@ class SessionsController < ApplicationController
 
   def destroy
     reset_session
+    @current_user = nil
     redirect_to root_path, notice: "Logged out!"
   end
 end
