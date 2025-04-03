931b0dfb1de73ce339a9ab03df935825abe71b66
Anxile <onebox.chen@outlook.com>
2025-04-02 23:57:49 -0400

    Add functionality of publishing posts to the system.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,16 @@
 class PostsController < ApplicationController
     def index
+        @posts = Post.all
+    end
+
+    def show
+        @post = Post.find_by(params[:id])
+    end
+
+    def new
+        @post = Post.new
+    end
+    def create
+        @post = Post.create(title: params[:post][:title], content: params[:post][:content], user_id: current_user.id)
     end
 end
