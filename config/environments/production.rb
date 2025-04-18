3ff6d0be8c19925b4bd95f05332cb19efd2470e8
Anxile <onebox.chen@outlook.com>
2025-04-17 23:23:36 -0400

    Configure SMTP settings for Action Mailer in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "my_portfolio_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address:              Rails.application.credentials.dig(:gmail, :address),
+  port:                 Rails.application.credentials.dig(:gmail, :port),
+  user_name:            Rails.application.credentials.dig(:gmail, :user_name),
+  password:             Rails.application.credentials.dig(:gmail, :password),
+  authentication:       'plain',
+  enable_starttls_auto: Rails.application.credentials.dig(:gmail, :enable_starttls_auto)
+}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
