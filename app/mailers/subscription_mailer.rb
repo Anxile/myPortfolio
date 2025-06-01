af2e8a0af39f970649e7744e3ce5b4f5bd514740
Anxile <onebox.chen@outlook.com>
2025-06-01 17:07:01 -0400

    Update mail template

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -2,7 +2,7 @@ class SubscriptionMailer < ApplicationMailer
     def subscribe(user, signed_url)
         @user = user
         @signed_url = signed_url
-        mail(to: @user.email, subject: "Subscription Confirmation")
+        mail(to: @user.email, subject: "Here's Yihe's Resume")
     end
     def unsubscribe
         mail(to: @user.email, subject: 'Unsubscription Confirmation')
