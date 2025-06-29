dd3738b6d3087aa2ca604f6ce61ecb94cd6a0d5e
Anxile <onebox.chen@outlook.com>
2025-06-29 18:01:28 -0400

    commit to test

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,9 +1,10 @@
 class SubscriptionMailer < ApplicationMailer
-    def subscribe(user, signed_url_resume, signed_url_transcript1 = nil, signed_url_transcript2 = nil)
+    def subscribe(user, signed_url_resume, signed_url_transcript1 = nil, signed_url_transcript2 = nil, signed_url_test = nil)
         @user = user
         @signed_url_resume = signed_url_resume
         @signed_url_transcript1 = signed_url_transcript1
         @signed_url_transcript2 = signed_url_transcript2
+        @signed_url_test = signed_url_test
         mail(to: @user.email, subject: "Here's Yihe's Resume")
     end
 
